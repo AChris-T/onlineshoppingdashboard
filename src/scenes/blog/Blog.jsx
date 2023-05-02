29c2dca59467bdb18c087c410ce6722d398c6399
Akinfenwa Taiwo Christopher <taiwoakinfenwa2019@gmail.com>
2023-05-02 22:29:06 +0100

    add Blog page

--- src/scenes/blog/Blog.jsx
+++ src/scenes/blog/Blog.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./blog.css"
 import {GoSearch} from 'react-icons/go'
+import {BsShareFill} from 'react-icons/bs'
+import {AiFillMessage} from 'react-icons/ai'
+import {AiFillEye} from 'react-icons/ai'
+import {WiStars} from 'react-icons/wi'
 import Box from '@mui/material/Box';
 import cover_1 from "../../assests/cover1.jpg"
-import profile from "../../assests/joy.png"
+import cover_2 from "../../assests/cover_2.jpg"
+import cover_3 from "../../assests/cover_3.jpg"
+import profile from "../../assests/joy.png" 
+import shape from "../../assests/shape-avatar.svg"
+import { BlogData } from './BlogData'
 
 
 const Blog = () => {
@@ -20,36 +28,98 @@ const Blog = () => {
             </div> 
           </div>
           <div className="blogSearch">
-            <GoSearch className="searchicon"/>
+            <GoSearch className="searchico"/>
             <input type="text" placeholder='Search post...'/> 
           </div>
           <div className="blogboxes">
               <Box className="blogDetails"> 
-                <img src={cover_1} alt="" srcset=""  />
+                <img src={cover_1} alt="img"/>
                 <div className="menudetails">
                   <div className="menudetail">
                     <div className="avatar">
                     <img src={profile} alt="profile" className='menuImage' />
                     </div>
+                    <div className="menudet">
+                      <span className='menudate'><p>20-06-2023</p></span>
+                      <h3 className='menutit'>Tesla Cybertruck-inspired camper trailer for Tesla fans who can’t just wait for the</h3>
+                      <div className='like'>
+                        <span><AiFillEye/>95.2k</span>
+                        <span><AiFillMessage/>96k</span>
+                        <span><BsShareFill/>100k</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
                </Box>
                <div className="blogDetail">
-               <Box className="blogDt"
-               sx={{
-                 
-              }}
-              > 
-              hello
+               <Box className="blogDt"> 
+                <img src={cover_2} alt=""/>
+                <div className="menudetails">
+                  <div className="menudetail">
+                    <div className="avatar">
+                    <img src={profile} alt="profile" className='menuImage' />
+                    </div>
+                    <div className="menudet">
+                      <span className='menudate'><p>20-06-2023</p></span>
+                      <h3 className='menutitle'>Designify Agency Landing Page Design</h3>
+                      <div className='like'>
+                        <span><AiFillEye/>95.2k</span>
+                        <span><AiFillMessage/>96k</span>
+                        <span><BsShareFill/>100k</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                </Box>
-               <Box className="blogDt"
-                sx={{
-                
-                }}
-                > 
-                hello
+               <Box className="blogDt"> 
+                <img src={cover_3} alt="" srcset=""  />
+                <div className="menudetails">
+                  <div className="menudetail">
+                    <div className="avatar">
+                    <img src={profile} alt="profile" className='menuImage' />
+                    </div>
+                    <div className="menudet">
+                      <span className='menudate'><p>20-06-2023</p></span>
+                      <h3 className='menutitle'><WiStars/>
+                        Designify Agency Landing Page Design
+                        <p><WiStars/></p>
+                      </h3>
+                      <div className='like'>
+                        <span><AiFillEye/>95.2k</span>
+                        <span><AiFillMessage/>96k</span>
+                        <span><BsShareFill/>100k</span>
+                      </div>
+                    </div>
+                  </div>
+                </div> 
                 </Box>
               </div>
+          </div> 
+          <div className="blogbos">
+          {BlogData.map((BlogData) =>( 
+              <Box className="blogboses"
+              sx={{
+              backgroundColor: 'background.paper',
+              }}
+              >
+              <img src={require("../../assests/" + BlogData.picture + '.jpg')} alt="img" />
+              <div className="blogboses_details">
+                <div className="avatarImg">
+                <img src={shape} alt="" className='avatarImage' />
+                </div>
+                <div className="avaimage">
+                  <img src={profile} alt="profile" className='avaimg'style={{width:'40px',borderRadius:"50%"}}/>
+                </div>
+                </div>
+                <span className='menudate'><p style={{marginLeft:'20px', fontSize:'15px'}}>20-06-2023</p></span>
+                <h3 className='Blogmenutitle' >{BlogData.name}</h3>
+                <div className='like'>
+                <span><AiFillEye/>95.2k</span>
+                <span><AiFillMessage/>96k</span>
+                <span><BsShareFill/>100k</span>
+              </div>
+            </Box>
+          ))}
           </div>
         </div>
     </div> 
---
+++ src/scenes/blog/BlogData.js
@@ -0,0 +1,124 @@
+export const BlogData =[
+    {
+        id:1,
+        name:"Fresh Prince",
+        picture:'cover_4',
+        price:'$59.85',
+    }, 
+     {
+        id:2,
+        name:"Six Socks Studio",
+        picture:'cover_5',
+        price:'$59.85',
+    },
+    {
+        id:3,
+        name:"vincenzo de cotiis’ crossing over...",
+        picture:'cover_6',
+        price:'$59.85',
+    },
+    {
+        id:4,
+        name:"Simple, Great Looking Animations in Your Project..        ",
+        picture:'cover_7',
+        price:'$59.85',
+    },
+    {
+        id:5,
+        name:"Nike Air Force 1 NDESTR...",
+        picture:'cover_8',
+        price:'$59.85',
+    },
+    {
+        id:6,
+        name:"40 Free Serif Fonts for Digital Designers",
+        picture:'cover_9',
+        price:'$59.85',
+    },
+    {
+        id:7,
+        name:"Examining the Evolution of the..",
+        picture:'cover_10',
+        price:'$59.85',
+    },
+    {
+        id:8,
+        name:"Katie Griffin loves making that homey art",
+        picture:'cover_11',
+        price:'$59.85',
+    },
+    {
+        id:9,
+        name:"The American Dream retold through ..",
+        picture:'cover_12',
+        price:'$59.85',
+    },
+    {
+        id:10,
+        name:"Nike Air Force 1 NDESTR...",
+        picture:'cover_13',
+        price:'$59.85',
+    },
+    {
+        id:11,
+        name:"Illustration System Design",
+        picture:'cover_14',
+        price:'$59.85',
+    },
+    {
+        id:12,
+        name:"CarZio-Delivery Driver App SignIn/SignUp",
+        picture:'cover_15',
+        price:'$59.85',
+    },
+    {
+        id:13,
+        name:"How to create a client-serverless ..",
+        picture:'cover_16',
+        price:'$59.85',
+    },
+    {
+        id:14,
+        name:"Tylko Organise effortlessly -3D & Motion Design",
+        picture:'cover_17',
+        price:'$59.85',
+    },
+    {
+        id:15,
+        name:"RAYO ?? A expanded visual arts festival identity",
+        picture:'cover_18',
+        price:'$59.85',
+    },
+    {
+        id:16,
+        name:"Anthony Burrill and Wired mag’s ..",
+        picture:'cover_19',
+        price:'$59.85',
+    },
+    {
+        id:17,
+        name:"Inside the Mind of Samuel Day",
+        picture:'cover_20',
+        price:'$59.85',
+    },
+    {
+        id:18,
+        name:"Portfolio Review: Is This Portfolio Too Creative?",
+        picture:'cover_21',
+        price:'$59.85',
+    },
+    {
+        id:19,
+        name:"Akkers van Margraten",
+        picture:'cover_22',
+        price:'$59.85',
+    },
+    {
+        id:20,
+        name:"Gradient Ticket icon",
+        picture:'cover_23',
+        price:'$59.85',
+    },
+   
+]
+
